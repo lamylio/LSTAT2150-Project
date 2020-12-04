68e915c34e58e7515b5eea79c41a056439749c32
Lioche <lionel.lamy@student.uclouvain.be>
2020-12-04 13:26:43 +0100

    Investigation

---
+++ sources/investigate.R
@@ -0,0 +1,82 @@
+# Compute the MSE by M Monte-carlo simulations
+
+NW.investigation = function(M=5000, n=100){
+  
+  SE = matrix(NA, M, length(CP.to_check))
+  
+  for (i in 1:M){
+    boot.X = runif(n)
+    boot.Y = Y(boot.X)
+    SE[i,] = (sapply(CP.to_check,function(xi) NW.regEst(xi, boot.X, boot.Y, NW.h, Knorm))- m(CP.to_check))^2
+  }
+  
+  MSE = colMeans(SE)
+  return(MSE)
+}
+
+# ---
+
+LM.investigation = function(M=5000, n=100){
+  
+  SE = matrix(NA, M, length(CP.to_check))
+  
+  for (i in 1:M){
+    boot.X = runif(n)
+    # We need to sort in order to be able to retrieve the right x's
+    boot.Y = Y(boot.X)[order(boot.X)]
+    boot.X = sort(boot.X)
+    boot.reg = lm(boot.Y ~ poly(boot.X, degree = LM.poly))
+    boot.pred = fitted(boot.reg)[c(1,CP.to_check*n)]  # get em (add 1 bc index starts at 1 not 0)
+    SE[i,] = (boot.pred - m(CP.to_check))^2
+  }
+  
+  MSE = colMeans(SE)
+  return(MSE)
+}
+
+# ---
+# Create the dataframe for each sample size
+
+CP.investigation.toframe = function(investigation_method){
+  res = NULL
+  pb = progress_bar$new(
+    format = "[Investigation] Sample: :sample | [:bar] :current/:total |:percent | :elapsed", 
+    total = length(CP.investigation.samples)
+  )
+  pb$tick(0)
+  Sys.sleep(0.1)
+  for (s in CP.investigation.samples){
+    pb$tick(tokens=list(sample=s))
+    res = cbind(res, investigation_method(n=s))
+  }
+  return (res)
+}
+
+
+# ---
+# Drawing the evolution of the MSE
+
+CP.investigation.drawMSE = function(investigation_frame, title){
+  
+  middle = nrow(investigation_frame)-1
+  
+  plot(CP.investigation.samples, 
+    head(investigation_frame, 1), col = 1, lwd = 1, lty=2,
+    type="l", xlab="Sample size", ylab="MSE"
+  )
+  
+  for (i in 2:middle){
+    lines(CP.investigation.samples, investigation_frame[i,], col = i, lwd = 1)
+  }
+  
+  lines(CP.investigation.samples, tail(investigation_frame, 1), col = 1, lwd = 1, lty=3)
+  abline(h = 0, lty = 2)
+  legend("topright", 
+    legend = CP.to_check, 
+    col = c(1, 2:middle, 1),
+    lty = c(2, rep(1,middle-1), 3),
+    lwd = 2,
+    cex = 0.8
+  )
+  title(title)
+}
--- sources/minimization.r
+++ sources/minimization.r
@@ -1,12 +1,6 @@
-###
-# Made by Lionel Lamy
-# 
-# Nadayara-Watson & Linear Model with poly
-# Returns: minimized MISE/MSSE and bandwidth/poly associated
-##
-
 library(progress)
 
+# Only return the minimized MISE and the bandwidth associated
 NW.minimizeMISE = function(Kernel = Knorm, h.test = seq(0.01, 0.15, 0.001), M = 100, ni = 100){
   best = Inf
   best_h = 0
@@ -43,10 +37,11 @@ NW.minimizeMISE = function(Kernel = Knorm, h.test = seq(0.01, 0.15, 0.001), M = 
   return (c(best, best_h))
 }
 
-
+# This one returns MSSE, bandwidth, Bias and also the mean of the regressions (for variance)
 NW.minimizeMSSE = function(Kernel = Knorm, h.test = seq(0.01, 0.15, 0.001), M = 100, ni = 100){
-  best = Inf
-  best_h = 0
+  
+  boot.x = seq(0, 1, length = ni)
+  res = data.frame(MSSE=Inf, h=NA, Bias=matrix(NA, ni, 1), MReg=matrix(NA, ni, 1))
   
   pb = progress_bar$new(
     format = "[NW MSSE] Best: :bmsse (:bh) | Current: :cmsse (:ch) [:bar] :current/:total |:percent | :elapsed", 
@@ -56,33 +51,41 @@ NW.minimizeMSSE = function(Kernel = Knorm, h.test = seq(0.01, 0.15, 0.001), M = 
   pb$tick(0)
   for (h in h.test){
     SE = matrix(NA, M, ni)
+    Reg = matrix(NA, M, ni)
+    
     for (i in 1:M){
-      boot.x = seq(0, 1, length = ni) 
+      
       boot.X = runif(ni)
       boot.Y = Y(boot.X)
-      SE[i,] = (sapply(boot.x, function(xi) NW.regEst(xi, boot.X, boot.Y, h, Kernel)) - m(boot.x))^2
+      boot.reg = sapply(boot.x, function(xi) NW.regEst(xi, boot.X, boot.Y, h, Kernel))
+      Reg[i,] = boot.reg
+      SE[i,] = (boot.reg - m(boot.x))^2
     }
 
+    MReg = colMeans(Reg)
+    Bias = MReg - m(boot.x)
     MSE = colMeans(SE)
     MSSE = round(mean(MSE),6)
     
-    if (MSSE < best){
-      best = MSSE
-      best_h = h
+    if (MSSE < mean(res$MSSE)){
+      res$MSSE = MSSE
+      res$h = h
+      res$Bias = Bias
+      res$MReg = MReg
     }
-    pb$tick(tokens=list(bmsse=best, bh=best_h, cmsse=MSSE, ch=h))
+    pb$tick(tokens=list(bmsse=res$MSSE, bh=res$h, cmsse=MSSE, ch=h))
   }
-  sprintf("[NW MSSE]: %.6f with %.6f as bandwhidth", best, best_h)
-  return (c(best, best_h))
+  return (res)
 }
 
 # -----
 # Haven't found a way to compute MISE with LM
 # But as MSSE ~= MISE, this is fine.
-
-LM.minimizeMSSE = function(poly.test = seq(5, 20, 0.01), M = 200, ni = 100, raw=F){
-  best = Inf
-  best_poly = 0
+# This one returns MSSE, bandwidth, Bias and also the mean of the regressions (for variance)
+LM.minimizeMSSE = function(poly.test = seq(1, 20, 1), M = 200, ni = 100, raw=F){
+  
+  boot.X = runif(ni)
+  res = data.frame(MSSE=Inf, poly=NA, Bias=matrix(NA, ni, 1), MReg=matrix(NA, ni, 1))
   
   pb = progress_bar$new(
     format = "[LM MSSE] Best: :bmsse (:bp) | Current: :cmsse (:cp) [:bar] :current/:total |:percent | :elapsed", 
@@ -92,21 +95,27 @@ LM.minimizeMSSE = function(poly.test = seq(5, 20, 0.01), M = 200, ni = 100, raw=
   pb$tick(0)
   for (p in poly.test){
     SE = matrix(NA, M, ni)
+    Reg = matrix(NA, M, ni)
+    
     for (i in 1:M){
-      boot.X = runif(ni)
       boot.Y = Y(boot.X)
-      SE[i,] = (lm(boot.Y ~ poly(boot.X, degree = p, raw=raw))$fitted - m(boot.X))^2
+      boot.reg = lm(boot.Y ~ poly(boot.X, degree = p, raw=raw))$fitted
+      SE[i,] = (boot.reg - m(boot.X))^2
+      Reg[i,] = boot.reg
     }
     
+    MReg = colMeans(Reg)
+    Bias = MReg - m(boot.X)
     MSE = colMeans(SE)
     MSSE = round(mean(MSE),6)
     
-    if (MSSE < best){
-      best = MSSE
-      best_poly = p
+    if (MSSE < mean(res$MSSE)){
+      res$MSSE = MSSE
+      res$poly = p
+      res$Bias = Bias
+      res$MReg = MReg
     }
-    pb$tick(tokens=list(bmsse=best, bp=best_poly, cmsse=MSSE, cp=p))
+    pb$tick(tokens=list(bmsse=res$MSSE, bp=res$poly, cmsse=MSSE, cp=p))
   }
-  sprintf("[LM MSSE]: %.6f with %f as poly", best, best_poly)
-  return (c(best, best_poly))
+  return (res)
 }
